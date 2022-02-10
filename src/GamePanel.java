8ab8e32638067e289e632c403857c1588d281f21
Eluta Cline <79481227+tulipfactory@users.noreply.github.com>
2022-02-09 16:12:03 -0800

    added apple

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -30,12 +30,32 @@ public class GamePanel extends JPanel implements ActionListener {
 		
 	}
 	public void startGame() {
+		newApple();
+		running = true;
+		timer = new Timer(DELAY,this);
+		timer.start();
 		
 	}
 	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		draw(g);
 		
 	}
 	public void draw(Graphics g) {
+		
+		for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+		}//size of the apple
+		g.setColor(Color.red);
+		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+		
+	}
+	public void newApple() {
+		appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+		//allows apple to be places evenly thruout the screen.
+		//it will appear randomly each time
 		
 	}
 	public void move() {
