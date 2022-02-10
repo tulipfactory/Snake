b48b71e5d67d886bc15ae786f9966f1c64056d93
Eluta Cline <79481227+tulipfactory@users.noreply.github.com>
2022-02-09 16:45:45 -0800

    add gameover statement

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -133,7 +133,17 @@ public class GamePanel extends JPanel implements ActionListener{
 			timer.stop();
 		}
 	}
-	
+	public void gameOver(Graphics g) {
+		//Score
+		g.setColor(Color.red);
+		g.setFont( new Font("Ink Free",Font.BOLD, 40));
+		FontMetrics metrics1 = getFontMetrics(g.getFont());
+		g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+		//Game Over text
+		g.setColor(Color.red);
+		g.setFont( new Font("Ink Free",Font.BOLD, 75));
+		FontMetrics metrics2 = getFontMetrics(g.getFont());
+		g.drawString("Game Over", (SCREEN_WIDTH - metrics2.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
